91bfbfa14e3eb7c1af66eddb509203422440a87f
Harshit8172 <65771892+Harshit8172@users.noreply.github.com>
2023-07-24 12:58:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 import socket
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-def get_user_private_ip_address():
-    request = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    request.connect(("8.8.8.8", 53))
-    ip_address = request.getsockname()[0]
-    request.close()
-
-    private_ip_prefixes = ('10.', '172.', '192.')
-    for prefix in private_ip_prefixes:
-        if ip_address.startswith(prefix):
-            return ip_address
-
-    return None
 
 @app.route('/')
-def index():
-    private_ip = get_user_private_ip_address()
-    return render_template('index.html', private_ip=private_ip)
+def get_public_ip():
+    # Get the client's public IP address from the request headers
+    client_ip = request.remote_addr
+    print("IP address is =", client_ip)
+    return f"Hello! Your Public IP Address: {client_ip}"
 
 if __name__ == '__main__':
     app.run(debug=True)
